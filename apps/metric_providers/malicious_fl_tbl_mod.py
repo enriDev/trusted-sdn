122b4dda8308316785ab62a8b204e95190b14a34
enrico <enri.pancino@gmail.com>
2016-02-18 14:49:40 +0100

    Added trust collectro for malicious tbl mod
    TODO:
     - handle/store/raise the malicious flow table detection

--- apps/metric_providers/malicious_fl_tbl_mod.py
+++ apps/metric_providers/malicious_fl_tbl_mod.py
@@ -5,31 +5,40 @@ Created on Feb 9, 2016
 @version: 1.0
 '''
 
+import logging
+import sys
+import os
+import inspect
 
 from ryu.topology import event
 from ryu.topology.api import get_switch
 from ryu.controller import ofp_event
+from ryu.controller.handler import set_ev_cls
 from ryu.controller.handler import MAIN_DISPATCHER
 from ryu.lib import hub
 
 
-from trust_proprieties_monitor import TrustMonitorBase, EventTrustUpdate
-from apps.ofp_table_mod.of_tbl_mod_provider import OFTblModProvider
+from trust_collector_base import TrustCollectorBase, EventTrustUpdate
+from ofp_table_mod.of_tbl_mod_provider import OFTblModProvider  
+# require add apps to PYTHONPATH
+#TODO find better solution for imports
+
 
 
 LOG = logging.getLogger(__name__)
 
 
 
-class MaliciousFlTblMod(TrustMonitorBase):
+class MaliciousFlTblMod(TrustCollectorBase):
     
     FLOW_TBL_REQUEST_INTERVAL = 10
     
     def __init__(self):
         
         super(MaliciousFlTblMod, self).__init__('malicious_fl_tbl_mod')
-        self.switch_list = {}   # dpid -> datapath
-        self.flow_tbl_cache = OFTblModProvider().flow_table_cache
+        self.switch_list = {}                                   # dpid -> datapath
+        self.flow_cache = OFTblModProvider().flow_table_cache   # reference to flow table cache
+         
         self.threads.append( hub.spawn_after(self.LOAD_TIME, self.flow_tbl_monitoring_loop) )
         
     
@@ -47,7 +56,7 @@ class MaliciousFlTblMod(TrustMonitorBase):
             for datapath in self.switch_list.values():
                 self.flow_tbl_status_request(datapath)
             hub.sleep(self.FLOW_TBL_REQUEST_INTERVAL)
-
+            #TODO query the flow tables
 
 
     def flow_tbl_status_request(self, datapath):
@@ -65,27 +74,53 @@ class MaliciousFlTblMod(TrustMonitorBase):
         
         
     @set_ev_cls(ofp_event.EventOFPFlowStatsReply, MAIN_DISPATCHER)
-    def fl_tbl_status_reply_handler(self, ev):
+    def check_flow_table(self, ev):
         
         msg = ev.msg
-        datapath = msg.datapath
+        dpid = msg.datapath.id
         body = msg.body
         
-        flow_entries = []
-        for stat in body:
-            flow_entries.append(
-                                'dp=%s -- '
-                                'table_id=%s -- '
-                                'priority=%s -- '
-                                'idle_timeout=%s -- '
-                                'match=%s -- '
-                                'instruction=%s -- ' %
-                                (datapath.id,
-                                 stat.table_id,
-                                 stat.priority,
-                                 stat.idle_timeout,
-                                 stat.match,
-                                 stat.instructions,
-                                 ))
+        try:
+            cached_flow_table = self.get_cached_hashed_fl_tbl(dpid)
+            flow_table = {}
+        
+            for stat in body:
+                flow_entry = (int (stat.table_id),
+                              int (stat.priority),
+                              int (stat.idle_timeout),
+                              str (stat.match),
+                              str (stat.instructions),
+                              )
+                flow_hash = hash(flow_entry)
+                flow_table[flow_hash] = flow_entry
+                cached_flow_table[flow_hash]
+        
+        except KeyError as e:
+            # if the hash key is not found in the cached_flow_table dict,
+            # the flow entry was not present in the cached
+            LOG.info('\nMALICIOUS_MOD: Found flow table inconsistency in dp %s :\n'
+                     '%s', dpid, flow_table[e.args[0]])
+        
+        
+        
+    def get_cached_hashed_fl_tbl(self, dpid):
+        
+            stored_flow_table = self.flow_cache.flow_table_query(dpid)
+            cached_flow_table = {}
+            for flow in stored_flow_table:
+                flow_hash = hash(flow)
+                cached_flow_table[flow_hash] = flow
             
-        LOG.info('\nFlow table status: \n%s', flow_entries)
+            #LOG.info('\nCached flow table %s: \n%s', dpid, self.dict_to_str(cached_flow_table))
+            return cached_flow_table
+            
+        
+    def dict_to_str(self, dict):
+        stri = ''
+        for elem in dict.keys():
+            stri = stri + str(elem) + ' : ' + str(dict[elem]) + '\n'
+        return stri
+        
+        
+        
+        
